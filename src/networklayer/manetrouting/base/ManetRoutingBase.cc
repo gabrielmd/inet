255bace16e2d45c2f48bc29b758ed939d380d2d3
Levente Mészáros <levy@omnetpp.org>
2013-04-26 16:14:49 +0200

    IPv4/IPv6: dynamic registration of upper layer protocols; merge IPv?NetworkLayer's transport gates
    TCP, UDP, SCTP, RSVP, OSPF, MANET are all merged into transport gates.
    Network user modules must register their protocol number by sending
    an IPRegisterProtocolCommand from initialize.
    
    Introduced an IPSocket class that wraps the registration process.

--- src/networklayer/manetrouting/base/ManetRoutingBase.cc
+++ src/networklayer/manetrouting/base/ManetRoutingBase.cc
@@ -22,6 +22,7 @@
 
 #include "ManetRoutingBase.h"
 #include "UDPPacket.h"
+#include "IPSocket.h"
 #include "IPv4Datagram.h"
 #include "IPv4ControlInfo.h"
 #include "IPv4InterfaceData.h"
@@ -287,6 +288,8 @@ void ManetRoutingBase::registerRoutingModule()
 
 
  //   WATCH_MAP(*routesVector);
+   IPSocket socket(gate("to_ip"));
+   socket.registerProtocol(IP_PROT_MANET);
 }
 
 ManetRoutingBase::~ManetRoutingBase()
